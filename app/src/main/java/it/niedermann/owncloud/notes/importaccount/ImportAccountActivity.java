2fc13972b75f8bee56fd408e1c326a41ca0f9e7e
Noah Watson <nwatson3@illinois.edu>
2021-11-19 19:22:42 -0600

    Add "use offline" button

--- app/src/main/java/it/niedermann/owncloud/notes/importaccount/ImportAccountActivity.java
+++ app/src/main/java/it/niedermann/owncloud/notes/importaccount/ImportAccountActivity.java
@@ -73,6 +73,9 @@ public class ImportAccountActivity extends AppCompatActivity {
                 AccountImporter.requestAndroidAccountPermissionsAndPickAccount(this);
             }
         });
+        binding.useOfflineButton.setOnClickListener((v) -> {
+
+        });
     }
 
     @Override
